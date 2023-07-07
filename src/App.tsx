293a97db8aca7b860c5ddc8691adb3f1d720a7d2
Andrei <andrei.cherciu24@gmail.com>
2023-07-07 19:33:08 +0100

    First commit after project created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./fonts/montserrat/Montserrat-Bold.ttf";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="navbar-container">
+        <div className="navbar">
+          <div className="image-container">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/800px-Starbucks_Corporation_Logo_2011.svg.png"
+              alt="logo"
+            />
+          </div>
+          <div className="time-container">
+            <h1 className="time-header">11:52am</h1>
+            <span className="time-desc">
+              {new Intl.DateTimeFormat("en-GB", {
+                dateStyle: "long",
+              }).format(new Date(Date.now()))}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="meeting-container">
+        <h1>Meeting room one</h1>
+      </div>
+      <div className="progress-container">
+        <h4>Meeting in progress</h4>
+        <h1 className="progress_time-text">11:30am - 12:30pm</h1>
+        <h2 className="progress-title">Online Strategy</h2>
+      </div>
     </div>
   );
 }
