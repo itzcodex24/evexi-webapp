04d8e8daeab92e59255b8547ab518145526a4bed
Andrei <andrei.cherciu24@gmail.com>
2023-07-27 14:19:15 +0100

    Added fallback values to CSS variables

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Progress from "./components/progress";
 
 function App({ config }: { config: any }) {
   const { API_KEY, CID, LOGO, TEXT, error } = config;
-  console.log(`error : ${error}`);
 
   const now = new Date(Date.now());
   const tomorrow = now.getTime() + 60 * 60 * 24 * 1000;
