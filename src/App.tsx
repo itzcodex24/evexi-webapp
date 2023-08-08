3d270123a65c8cd2edf839d46b27b480c55e5f86
Andrei <andrei.cherciu24@gmail.com>
2023-08-08 10:33:25 +0100

    Resolves C3.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App({ config }: { config: any }) {
 
   const [loading, events, _error, vacant] = useAxios<EventAPI>({
     method: "GET",
-    url: `https://www.googleapis.com/calendar/v3/calendars/${CID}/events?key=${API_KEY}&orderBy=startTime&singleEvents=true&maxResults=5&timeMin=${now.toISOString()}&timeMax=${new Date(
+    url: `https://www.googleapis.com/calendar/v3/calendars/${CID}/events?key=${API_KEY}&orderBy=startTime&singleEvents=true&timeMin=${now.toISOString()}&timeMax=${new Date(
       tomorrow,
     ).toISOString()}`,
   });
