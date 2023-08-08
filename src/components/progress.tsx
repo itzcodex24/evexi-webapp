9c0e74ccabacd71a5cfae12b1d0788c217052ee1
Andrei <andrei.cherciu24@gmail.com>
2023-08-08 14:23:07 +0100

    Reworked progress component with new data from the API response.

--- src/components/progress.tsx
+++ src/components/progress.tsx
@@ -1,44 +1,44 @@
+import { useMemo } from "react";
 import { formatTime } from "../helpers/format-time";
+import { getDateDifference } from "../helpers/getDateDifference";
+import { isBetweenTimes } from "../helpers/isBetweenTimes";
 
-type ProgressProps = {
-  vacant: boolean | string;
-  events: EventAPI;
-};
-
-export default function Progress(props: ProgressProps) {
-  const { vacant, events } = props;
-
+export default function Progress({ events }: { events: EventItem[] }) {
   return (
     <div className="progress-container">
-      {vacant ? (
-        <>
-          <h4>Vacant for the next</h4>
-          <h1 className="progress_time-text">{vacant}</h1>
-        </>
-      ) : (
-        <>
-          {events.items.length === 0 ? (
-            <h4 className="progress_time-text">No upcoming events</h4>
-          ) : (
-            <>
-              <h4>Meeting in progress</h4>
-              <h1 className="progress_time-text">
-                {events.items[0] &&
-                  formatTime(
-                    new Date(Date.parse(events.items[0]["start"]["dateTime"])),
-                  )}
-                -{" "}
-                {formatTime(
-                  new Date(Date.parse(events.items[0]["end"]["dateTime"])),
+      {events.length > 0 ? (
+        isBetweenTimes(
+          Date.parse(events[0].start.dateTime),
+          Date.now(),
+          Date.parse(events[0].end.dateTime),
+        ) ? (
+          <>
+            <h4>Meeting in progress</h4>
+            <h1 className="progress_time-text">
+              {events[0] &&
+                formatTime(
+                  new Date(Date.parse(events[0]["start"]["dateTime"])),
                 )}
-              </h1>
+              - {formatTime(new Date(Date.parse(events[0]["end"]["dateTime"])))}
+            </h1>
 
-              <h2 className="progress-title clamp-1">
-                {events.items[0] && events.items[0].summary}
-              </h2>
-            </>
-          )}
-        </>
+            <h2 className="progress-title clamp-1">
+              {events[0].summary ?? "Untitled Event"}
+            </h2>
+          </>
+        ) : (
+          <>
+            <h4>Vacant for the next</h4>
+            <h1 className="progress_time-text">
+              {getDateDifference(
+                Date.parse(events[0].start.dateTime),
+                Date.now(),
+              )}
+            </h1>
+          </>
+        )
+      ) : (
+        <h4 className="progress_time-text">No upcoming meetings!</h4>
       )}
     </div>
   );
