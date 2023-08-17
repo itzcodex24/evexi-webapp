cca622ec5eba6fcdfe31c3ba5bb9b67c95a01118
Andrei <andrei.cherciu24@gmail.com>
2023-08-17 13:53:53 +0100

    resolves mutation issues with the events data

--- src/components/progress.tsx
+++ src/components/progress.tsx
@@ -2,10 +2,11 @@ import { formatTime } from "../helpers/format-time";
 import { getDateDifference } from "../helpers/getDateDifference";
 import { isBetweenTimes } from "../helpers/isBetweenTimes";
 
-export default function Progress({ events }: { events: EventItem[] }) {
-  let sortedEvents = events.sort(
-    (a, b) => Date.parse(a.created) - Date.parse(b.created),
-  );
+export default function Progress({
+  events,
+}: {
+  events: Readonly<EventItem[]>;
+}) {
   return (
     <div className="progress-container">
       {events.length > 0 ? (
@@ -17,20 +18,20 @@ export default function Progress({ events }: { events: EventItem[] }) {
           <>
             <h4 className="meeting-status">Meeting in progress</h4>
             <h1 className="progress_time-text">
-              {sortedEvents[0] &&
+              {events[0] &&
                 formatTime(
-                  new Date(Date.parse(sortedEvents[0]["start"]["dateTime"])),
-                  sortedEvents[0].start.timeZone,
+                  new Date(Date.parse(events[0]["start"]["dateTime"])),
+                  events[0].start.timeZone,
                 )}
               -{" "}
               {formatTime(
-                new Date(Date.parse(sortedEvents[0]["end"]["dateTime"])),
-                sortedEvents[0].end.timeZone,
+                new Date(Date.parse(events[0]["end"]["dateTime"])),
+                events[0].end.timeZone,
               )}
             </h1>
 
             <h2 className="progress-title clamp-1">
-              {sortedEvents[0].summary ?? "Untitled Event"}
+              {events[0].summary ?? "Untitled Event"}
             </h2>
           </>
         ) : (
